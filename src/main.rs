5f6f9c7fa9056c3ab6f0ae7a9caceda173e5629b
vnrg <volodya-nrg@mail.ru>
2026-07-15 04:07:46 +0300

    add html

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@ mod configs;
 mod logger;
 
 use axum::Router;
+use axum::response::Html;
 use axum::routing::get;
 use std::error::Error;
 // use sqlx::Connection;
 use sqlx::FromRow;
 use tokio::net::TcpListener;
+use tower_http::services::fs::{ServeDir, ServeFile};
 
 #[tokio::main]
 async fn main() {
@@ -22,16 +24,23 @@ async fn run(config_filepath: &str) -> Result<(), Box<dyn Error>> {
     logger::init(cfg.service_name, cfg.version, cfg.log.level);
 
     let listener: TcpListener = TcpListener::bind(&cfg.http_server.address).await?;
-    let router: Router = Router::new().route("/", get(handler_main).with_state(State {}));
+    let router: Router = Router::new()
+        .route(
+            "/",
+            get(|| async { Html(include_str!("../web/index.html")) }),
+        )
+        .fallback_service(
+            ServeDir::new("./web")
+                // предварительно сжатые файлы
+                .precompressed_gzip()
+                .precompressed_br()
+                .not_found_service(ServeFile::new("./web/404.html")),
+        );
     // let conn = Connection::connect("sqlite:./data/db.sqlite3").await.unwrap();
 
     log::info!("start server on {}", &cfg.http_server.address);
     axum::serve(listener, router).await?;
     Ok(())
-}
-
-async fn handler_main() -> &'static str {
-    "Hello, world!"
 }
 
 #[derive(Debug, Clone)]
