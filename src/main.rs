e6a66acf23f2a8e79d14c7101ee3b0321ef7443b
vnrg <volodya-nrg@mail.ru>
2026-07-15 23:48:37 +0300

    update 1

--- src/main.rs
+++ src/main.rs
@@ -10,38 +10,16 @@ use tokio::net::TcpListener;
 use tokio::time::{Duration, sleep};
 use tower_http::services::fs::{ServeDir, ServeFile};
 
-fn main() {
-    let args = Args::parse();
-    let cfg = config::new(args.config.as_str()).expect("failed to load config file");
-
-    logger::init("my".to_string(), "1".to_string(), "info".to_string());
-    loop {
-        // println!(
-        //     "{} {} {} {}",
-        //     args.config, &cfg.service_name, &cfg.version, &cfg.log.level
-        // );
-        log::info!(
-            "{} {} {} {}",
-            &args.config,
-            &cfg.service_name,
-            &cfg.version,
-            &cfg.log.level
-        );
-        std::thread::sleep(Duration::from_secs(1));
-    }
-}
-
 #[tokio::main]
-async fn main2() {
+async fn main() {
     let args = Args::parse();
 
     if let Err(e) = run(args.config.as_str()).await {
         log::error!("failed to run app: {e}");
+        println!("================ 1 ================ ");
         std::process::exit(1);
     }
-
-    println!("sleep 2000");
-    sleep(Duration::from_secs(2000)).await;
+    println!("================ 2 ================ ");
 }
 
 async fn run(config_filepath: &str) -> Result<(), Box<dyn Error>> {
@@ -58,14 +36,15 @@ async fn run(config_filepath: &str) -> Result<(), Box<dyn Error>> {
         .fallback_service(
             ServeDir::new("./web")
                 // предварительно сжатые файлы
-                .precompressed_gzip()
-                .precompressed_br()
+                // .precompressed_gzip()
+                // .precompressed_br()
                 .not_found_service(ServeFile::new("./web/404.html")),
         );
     // let conn = Connection::connect("sqlite:./data/db.sqlite3").await.unwrap();
 
     log::info!("start server on {}", &cfg.http_server.address);
     axum::serve(listener, router).await?;
+    println!("================ 3 ================ ");
     Ok(())
 }
 
